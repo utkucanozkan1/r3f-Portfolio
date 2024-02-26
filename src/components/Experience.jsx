189d8b7d390b559afe610cda1df619f19d3b1316
Utku Ozkan <utkucozkan@gmail.com>
2024-02-25 23:02:02 -0500

    phase 1 complete

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,38 @@
-import { OrbitControls } from "@react-three/drei";
-
+/* eslint-disable react/no-unknown-property */
+import { OrbitControls, Sky, Environment, ContactShadows } from '@react-three/drei';
+import React from 'react';
+import { Avatar } from './Avatar';
+import { useControls } from 'leva';
 export const Experience = () => {
-  return (
-    <>
-      <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
-    </>
-  );
+	const { animation } = useControls({
+		animation: {
+			value: 'Typing',
+			options: ['Typing', 'Falling', 'Standing']
+		}
+	})
+	return (
+		<>
+			<OrbitControls />
+			<Sky />
+			<Environment preset='sunset' />
+			<group position-y={-1}>
+				<ContactShadows opacity={0.42} scale={10} blur={1} far={10} resolution={256} color='#000000' />
+				<Avatar animation={animation} />
+				{
+					animation === 'Typing' && (
+						<mesh scale={[0.8, 0.5, 0.8]} position-y={0.25}>
+							<boxGeometry />
+							<meshStandardMaterial color='white' />
+						</mesh>
+					)
+				}
+				<mesh scale={5}
+					rotation-x={-Math.PI * 0.5}
+					position-y={-0.0001}>
+					<planeGeometry />
+					<meshStandardMaterial color='white' />
+				</mesh>
+			</group>
+		</>
+	);
 };
