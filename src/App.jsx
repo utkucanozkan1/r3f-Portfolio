660bc9ea1bad4b7d76aea88b24fe7abb5108653b
Utku Ozkan <utkucozkan@gmail.com>
2024-03-05 08:32:02 -0500

    implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,58 @@
-import { Canvas } from "@react-three/fiber";
-import { Experience } from "./components/Experience";
+import { Scroll, ScrollControls } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import { MotionConfig } from 'framer-motion';
+import { Leva } from 'leva';
+import { useEffect, useState } from 'react';
+// import { Cursor } from './components/Cursor';
+import { Experience } from './components/Experience';
+// import { Interface } from './components/Interface';
+// import { Menu } from './components/Menu';
+// import { ScrollManager } from './components/ScrollManager';
+const framerMotionConfig = {
+	type: 'spring',
+	mass: 5,
+	stiffness: 500,
+	damping: 50,
+	restDelta: 0.0001
+};
 
-function App() {
-  return (
-    <Canvas shadows camera={{ position: [0,2,5], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
-      <Experience />
-    </Canvas>
-  );
+function App () {
+	const [section, setSection] = useState(0);
+	const [menuOpened, setMenuOpened] = useState(false);
+
+	useEffect(() => {
+		setMenuOpened(false);
+	}, [section]);
+
+	return (
+		<>
+			<MotionConfig
+				transition={{
+					...framerMotionConfig
+				}}
+			>
+				<Canvas shadows camera={{ position: [0, 3, 10], fov: 42 }}>
+					<color attach="background" args={['#e6e7ff']} />
+					<ScrollControls pages={4} damping={0.1}>
+						{/* <ScrollManager section={section} onSectionChange={setSection} /> */}
+						<Scroll>
+							<Experience section={section} menuOpened={menuOpened} />
+						</Scroll>
+						<Scroll html>
+							{/* <Interface setSection={setSection} /> */}
+						</Scroll>
+					</ScrollControls>
+				</Canvas>
+				{/* <Menu
+					onSectionChange={setSection}
+					menuOpened={menuOpened}
+					setMenuOpened={setMenuOpened}
+				/> */}
+				{/* <Cursor /> */}
+			</MotionConfig>
+			<Leva hidden />
+		</>
+	);
 }
 
 export default App;
